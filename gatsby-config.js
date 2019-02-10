75aa97c24c30462aeedfd1e71ca0873f34248a55
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-02-10 00:28:32 -0500

    fix: project path

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`
   },
-  pathPrefix: '/example-prime',
+  pathPrefix: '/example-gatsby',
   plugins: [
     {
       resolve: `gatsby-source-graphql-universal`,
