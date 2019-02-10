084fdd3c8f63205377e392e2d89d514ceba7915e
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-02-09 23:16:05 -0500

    refactor: prime utils

--- src/utils/prime.js
+++ src/utils/prime.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import { getIsolatedQuery } from 'gatsby-source-graphql-universal';
-import { client } from "./client";
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
+import { getPreviewHeaders } from "./prime";
+
+const httpLink = new HttpLink({
+  uri: new URL(global.___graphqlUniversal.prime.url),
+  fetch,
+});
+
+const withToken = setContext(async (input, b) => {
+  return {
+    headers: await getPreviewHeaders(),
+  };
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      );
+    });
+  }
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+
+export const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, withToken, httpLink]),
+  cache: new InMemoryCache()
+});
 
 export const getPreviewHeaders = async () => {
   const query = window.location.search
