a23a7b79d93c4b29286fac2d48744783f489bf85
itstimetogoleft <itstimetogoleft@gmail.com>
2019-02-20 05:25:34 -0800

    Removed deprecated pathContext in favor of pageContext

--- src/utils/prime.js
+++ src/utils/prime.js
@@ -1,27 +1,28 @@
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-import { PrimeLink, isPreviewing } from 'apollo-link-prime';
-import { getIsolatedQuery } from 'gatsby-source-graphql-universal';
-import React from 'react';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { isPreviewing, PrimeLink } from "apollo-link-prime";
+import { getIsolatedQuery } from "gatsby-source-graphql-universal";
+import React from "react";
 
 export const client = new ApolloClient({
   link: PrimeLink({
-    url: 'https://example-prime.herokuapp.com',
+    url: "https://example-prime.herokuapp.com"
   }),
   cache: new InMemoryCache()
 });
 
 export const usePreview = (props, query) => {
   const [data, setData] = React.useState(props.data);
-    React.useEffect(() => {
-      if (isPreviewing()) {
-        client.query({
-          query: getIsolatedQuery(query, 'prime', 'Prime'),
-          variables: props.pathContext,
+  React.useEffect(() => {
+    if (isPreviewing()) {
+      client
+        .query({
+          query: getIsolatedQuery(query, "prime", "Prime"),
+          variables: props.pageContext
         })
-        .then(({ data: prime }) => setData({ prime }))
-      }
-    }, []);
+        .then(({ data: prime }) => setData({ prime }));
+    }
+  }, []);
 
   return data;
-}
+};
